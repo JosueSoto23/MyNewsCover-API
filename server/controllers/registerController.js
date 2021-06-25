3b651216419bd95a92365ae756652780098e19d2
DanielAlfaro85 <geberthalfaro85@gmail.com>
2021-06-24 18:57:18 -0600

    Registrar usuarios(Incompleta)

---
+++ server/controllers/registerController.js
@@ -0,0 +1,164 @@
+const User = require("../models/registerModel");
+
+/**
+ * Crea los usuarios
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const coursePost = (req, res) => {
+  var user = new User();
+
+  user.name = req.body.name;
+  user.lastName = req.body.lastName;
+
+  user.email = req.body.email;
+  user.password = req.body.password;
+
+  user.address1 = req.body.address1;
+  user.address2 = req.body.address2;
+
+  user.country = req.body.country;
+  user.city = req.body.city;
+
+  user.postalCode = req.body.postalCode;
+  user.phoneNumber = req.body.phoneNumber;
+
+  if (user.name && user.lastName && user.email && user.password && user.address1 && user.address2 && user.country && user.city && user.postalCode && user.phoneNumber) {
+    user.save(function (err) {
+      if (err) {
+        res.status(422);
+        console.log('error while saving the user', err)
+        res.json({
+          error: 'There was an error saving the user'
+        });
+      }
+      res.status(201);//CREATEDa
+      res.header({
+        'location': `http://localhost:3000/api/courses/?id=${user.id}`
+      });
+      res.json(user);
+    });
+  } else {
+    res.status(422);
+    console.log('error while saving the user')
+    res.json({
+      error: 'No valid data provided for user'
+    });
+  }
+};
+
+/**
+ * Get all tasks
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const courseGet = (req, res) => {
+  // if an specific task is required
+  if (req.query && req.query.id) {
+    Course.findById(req.query.id, function (err, user) {
+      if (err) {
+        res.status(404);
+        console.log('error while queryting the user', err)
+        res.json({ error: "Course doesnt exist" })
+      }
+      res.json(user);
+    });
+  } else {
+    // get all tasks
+    Course.find(function (err, user) {
+      if (err) {
+        res.status(422);
+        res.json({ "error": err });
+      }
+      res.json(user);
+    });
+
+  }
+};
+
+/**
+ * Delete one task
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const courseDelete = (req, res) => {
+  // if an specific task is required
+  if (req.query && req.query.id) {
+    Course.findById(req.query.id, function (err, user) {
+      if (err) {
+        res.status(500);
+        console.log('error while queryting the user', err)
+        res.json({ error: "Course doesnt exist" })
+      }
+      //if the task exists
+      if(user) {
+        user.remove(function(err){
+          if(err) {
+            res.status(500).json({message: "There was an error deleting the user"});
+          }
+          res.status(204).json({});
+        })
+      } else {
+        res.status(404);
+        console.log('error while queryting the user', err)
+        res.json({ error: "Course doesnt exist" })
+      }
+    });
+  } else {
+    res.status(404).json({ error: "You must provide a Course ID" });
+  }
+};
+
+/**
+ * Updates a task
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const coursePatch = (req, res) => {
+  // get task by id
+  if (req.query && req.query.id) {
+    Course.findById(req.query.id, function (err, user) {
+      if (err) {
+        res.status(404);
+        console.log('error while queryting the user', err)
+        res.json({ error: "Course doesnt exist" })
+      }
+
+      // update the task object (patch)
+      user.name = req.body.name ? req.body.name : task.name;
+      user.code = req.body.code ? req.body.code : task.code;
+
+      user.career = req.body.career ? req.body.career : task.career;
+      user.credits = req.body.credits ? req.body.credits : task.credits;
+      // update the task object (put)
+      // task.title = req.body.title
+      // task.detail = req.body.detail
+
+      user.save(function (err) {
+        if (err) {
+          res.status(422);
+          console.log('error while saving the user', err)
+          res.json({
+            error: 'There was an error saving the user'
+          });
+        }
+        res.status(200); // OK
+        res.json(user);
+      });
+    });
+  } else {
+    res.status(404);
+    res.json({ error: "Course doesnt exist" })
+  }
+};
+
+module.exports = {
+  courseGet,
+  coursePost,
+  coursePatch,
+  courseDelete
+}
--- server/index.js
+++ server/index.js
@@ -7,19 +7,19 @@ const db = mongoose.connect("mongodb://127.0.0.1:27017/tasks-api");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const {
+/*const {
   taskPatch,
   taskPost,
   taskGet,
   taskDelete
-} = require("./controllers/taskController");
+} = require("./controllers/taskController");*/
 
 const {
-  coursePatch,
-  coursePost,
-  courseGet,
-  courseDelete
-} = require("./controllers/courseController");
+  userPatch,
+  userPost,
+  userGet,
+  userDelete
+} = require("./controllers/registerController");
 
 // check for cors
 const cors = require("cors");
@@ -29,16 +29,16 @@ app.use(cors({
 }));
 
 // listen to the task request
-app.get("/api/tasks", taskGet);
+/*app.get("/api/tasks", taskGet);
 app.post("/api/tasks", taskPost);
 app.patch("/api/tasks", taskPatch);
 app.put("/api/tasks", taskPatch);
-app.delete("/api/tasks", taskDelete);
+app.delete("/api/tasks", taskDelete);*/
 
-app.get("/api/courses", courseGet);
-app.post("/api/courses", coursePost);
-app.patch("/api/courses", coursePatch);
-app.put("/api/courses", coursePatch);
-app.delete("/api/courses", courseDelete);
+app.get("/api/users", userGet);
+app.post("/api/users", userPost);
+app.patch("/api/users", userPatch);
+app.put("/api/users", userPatch);
+app.delete("/api/users", userDelete);
 
 app.listen(3000, () => console.log(`Example app listening on port 3000!`))
