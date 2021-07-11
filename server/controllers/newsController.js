5b59e19044b53948e1b38fa264d738aa8e134df5
Josué Soto <deivysoto66@gmail.com>
2021-07-11 17:19:37 -0600

    News POST method

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,78 +1,74 @@
-const User = require("../models/newsModel");
+const News = require("../models/newsModel");
 
 /**
- * Crea las noticias
+ * Creates news
  *
  * @param {*} req
  * @param {*} res
  */
-const userPost = (req, res) => {
-  var user = new User();
+const newsPost = (req, res) => {
+  var news = new News();
 
-  user.firstName = req.body.firstName;
-  user.lastName = req.body.lastName;
+  news.title = req.body.title;
+  news.short_description = req.body.short_description;
 
-  user.email = req.body.email;
-  user.password = req.body.password;
+  news.permanlink = req.body.permanlink;
 
-  user.address1 = req.body.address1;
-  user.address2 = req.body.address2;
+  news.date = req.body.date;
+  news.news_source_id = req.body.news_source_id;
 
-  user.country = req.body.country;
-  user.city = req.body.city; 
+  news.user_id = req.body.user_id;
+  news.category_id = req.body.category_id; 
 
-  user.postalCode = req.body.postalCode;
-  user.phoneNumber = req.body.phoneNumber;
-
-  if (user.firstName && user.lastName && user.email && user.password) {
-    user.save(function (err) {
+  if (news.title && news.short_description && news.permanlink && news.news_source_id) {
+    news.save(function (err) {
       if (err) {
         res.status(422);
-        console.log('error while saving the user', err)
+        console.log('error while saving the news', err)
         res.json({
-          error: 'There was an error saving the user'
+          error: 'There was an error saving the news'
         });
       }
-      res.status(201);//CREATEDa
+      res.status(201);//CREATED
       res.header({
-        'location': `http://localhost:3000/api/users/?id=${user.id}`
+        'location': `http://localhost:3000/api/newss/?id=${news.id}`
       });
-      res.json(user);
+      res.json(news);
     });
   } else {
     res.status(422);
-    console.log('error while saving the user')
+    console.log('error while saving the news')
     res.json({
-      error: 'No valid data provided for user'
+      error: 'No valid data provided for news'
     });
   }
 };
 
 /**
- * Get all tasks
+ * Get all news
  *
  * @param {*} req
  * @param {*} res
  */
-const userGet = (req, res) => {
-  // if an specific task is required
+const newsGet = (req, res) => {
+  
   if (req.query && req.query.id) {
-    User.findById(req.query.id, function (err, user) {
+    news.findById(req.query.id, function (err, news) {
       if (err) {
         res.status(404);
-        console.log('error while queryting the user', err)
-        res.json({ error: "User doesnt exist" })
+        console.log('error while queryting the news', err)
+        res.json({ error: "news doesnt exist" })
       }
-      res.json(user);
+      res.json(news);
     });
   } else {
     // get all tasks
-    User.find(function (err, user) {
+    news.find(function (err, news) {
       if (err) {
         res.status(422);
         res.json({ "error": err });
       }
-      res.json(user);
+      res.json(news);
     });
 
   }
@@ -84,31 +80,31 @@ const userGet = (req, res) => {
  * @param {*} req
  * @param {*} res
  */
-const userDelete = (req, res) => {
+const newsDelete = (req, res) => {
   // if an specific task is required
   if (req.query && req.query.id) {
-    User.findById(req.query.id, function (err, user) {
+    news.findById(req.query.id, function (err, news) {
       if (err) {
         res.status(500);
-        console.log('error while queryting the user', err)
-        res.json({ error: "User doesnt exist" })
+        console.log('error while queryting the news', err)
+        res.json({ error: "news doesnt exist" })
       }
       //if the task exists
-      if(user) {
-        user.remove(function(err){
+      if(news) {
+        news.remove(function(err){
           if(err) {
-            res.status(500).json({message: "There was an error deleting the user"});
+            res.status(500).json({message: "There was an error deleting the news"});
           }
           res.status(204).json({});
         })
       } else {
         res.status(404);
-        console.log('error while queryting the user', err)
-        res.json({ error: "User doesnt exist" })
+        console.log('error while queryting the news', err)
+        res.json({ error: "news doesnt exist" })
       }
     });
   } else {
-    res.status(404).json({ error: "You must provide a User ID" });
+    res.status(404).json({ error: "You must provide a news ID" });
   }
 };
 
@@ -118,56 +114,56 @@ const userDelete = (req, res) => {
  * @param {*} req
  * @param {*} res
  */
-const userPatch = (req, res) => {
+const newsPatch = (req, res) => {
   // get task by id
   if (req.query && req.query.id) {
-    User.findById(req.query.id, function (err, user) {
+    news.findById(req.query.id, function (err, news) {
       if (err) {
         res.status(404);
-        console.log('error while queryting the user', err)
-        res.json({ error: "User doesnt exist" })
+        console.log('error while queryting the news', err)
+        res.json({ error: "news doesnt exist" })
       }
 
       // update the task object (patch)
-      user.firstName = req.body.name ? req.body.firstName : task.firstName;
-      user.lastName = req.body.lastName ? req.body.lastName : task.lastName;
+      news.title = req.body.name ? req.body.title : task.title;
+      news.short_description = req.body.short_description ? req.body.short_description : task.short_description;
     
-      user.email = req.body.email ? req.body.email : task.email;
-      user.password = req.body.password ? req.body.password : task.password;
+      news.permanlink = req.body.permanlink ? req.body.permanlink : task.permanlink;
+      news.password = req.body.password ? req.body.password : task.password;
     
-      user.address1 = req.body.address1 ? req.body.address1 : task.address1;
-      user.address2 = req.body.address2 ? req.body.address2 : task.address2;
+      news.date = req.body.date ? req.body.date : task.date;
+      news.news_source_id = req.body.news_source_id ? req.body.news_source_id : task.news_source_id;
     
-      user.country = req.body.country ? req.body.country : task.country;
-      user.city = req.body.city ? req.body.city : task.city;
+      news.user_id = req.body.user_id ? req.body.user_id : task.user_id;
+      news.category_id = req.body.category_id ? req.body.category_id : task.category_id;
     
-      user.postalCode = req.body.postalCode ? req.body.postalCode : task.postalCode;
-      user.phoneNumber = req.body.phoneNumber ? req.body.phoneNumber : task.phoneNumber;
+      news.postalCode = req.body.postalCode ? req.body.postalCode : task.postalCode;
+      news.phoneNumber = req.body.phoneNumber ? req.body.phoneNumber : task.phoneNumber;
       // update the task object (put)
       // task.title = req.body.title
       // task.detail = req.body.detail
 
-      user.save(function (err) {
+      news.save(function (err) {
         if (err) {
           res.status(422);
-          console.log('error while saving the user', err)
+          console.log('error while saving the news', err)
           res.json({
-            error: 'There was an error saving the user'
+            error: 'There was an error saving the news'
           });
         }
         res.status(200); // OK
-        res.json(user);
+        res.json(news);
       });
     });
   } else {
     res.status(404);
-    res.json({ error: "User doesnt exist" })
+    res.json({ error: "news doesnt exist" })
   }
 };
 
 module.exports = {
-  userGet,
-  userPost,
-  userPatch,
-  userDelete
+  newsGet,
+  newsPost,
+  newsPatch,
+  newsDelete
 }
