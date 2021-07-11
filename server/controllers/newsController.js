32adbb6ce86f42d8a5fee1707ecaa53c1ea75a79
Josué Soto <deivysoto66@gmail.com>
2021-07-11 17:24:15 -0600

    News Controller finished

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -53,7 +53,7 @@ const newsPost = (req, res) => {
 const newsGet = (req, res) => {
   
   if (req.query && req.query.id) {
-    news.findById(req.query.id, function (err, news) {
+    News.findById(req.query.id, function (err, news) {
       if (err) {
         res.status(404);
         console.log('error while queryting the news', err)
@@ -63,7 +63,7 @@ const newsGet = (req, res) => {
     });
   } else {
     // get all tasks
-    news.find(function (err, news) {
+    News.find(function (err, news) {
       if (err) {
         res.status(422);
         res.json({ "error": err });
@@ -75,21 +75,21 @@ const newsGet = (req, res) => {
 };
 
 /**
- * Delete one task
+ * Deletes on new
  *
  * @param {*} req
  * @param {*} res
  */
 const newsDelete = (req, res) => {
-  // if an specific task is required
+  
   if (req.query && req.query.id) {
-    news.findById(req.query.id, function (err, news) {
+    News.findById(req.query.id, function (err, news) {
       if (err) {
         res.status(500);
         console.log('error while queryting the news', err)
         res.json({ error: "news doesnt exist" })
       }
-      //if the task exists
+      
       if(news) {
         news.remove(function(err){
           if(err) {
@@ -108,62 +108,8 @@ const newsDelete = (req, res) => {
   }
 };
 
-/**
- * Updates a task
- *
- * @param {*} req
- * @param {*} res
- */
-const newsPatch = (req, res) => {
-  // get task by id
-  if (req.query && req.query.id) {
-    news.findById(req.query.id, function (err, news) {
-      if (err) {
-        res.status(404);
-        console.log('error while queryting the news', err)
-        res.json({ error: "news doesnt exist" })
-      }
-
-      // update the task object (patch)
-      news.title = req.body.name ? req.body.title : task.title;
-      news.short_description = req.body.short_description ? req.body.short_description : task.short_description;
-    
-      news.permanlink = req.body.permanlink ? req.body.permanlink : task.permanlink;
-      news.password = req.body.password ? req.body.password : task.password;
-    
-      news.date = req.body.date ? req.body.date : task.date;
-      news.news_source_id = req.body.news_source_id ? req.body.news_source_id : task.news_source_id;
-    
-      news.user_id = req.body.user_id ? req.body.user_id : task.user_id;
-      news.category_id = req.body.category_id ? req.body.category_id : task.category_id;
-    
-      news.postalCode = req.body.postalCode ? req.body.postalCode : task.postalCode;
-      news.phoneNumber = req.body.phoneNumber ? req.body.phoneNumber : task.phoneNumber;
-      // update the task object (put)
-      // task.title = req.body.title
-      // task.detail = req.body.detail
-
-      news.save(function (err) {
-        if (err) {
-          res.status(422);
-          console.log('error while saving the news', err)
-          res.json({
-            error: 'There was an error saving the news'
-          });
-        }
-        res.status(200); // OK
-        res.json(news);
-      });
-    });
-  } else {
-    res.status(404);
-    res.json({ error: "news doesnt exist" })
-  }
-};
-
 module.exports = {
   newsGet,
   newsPost,
-  newsPatch,
   newsDelete
 }
