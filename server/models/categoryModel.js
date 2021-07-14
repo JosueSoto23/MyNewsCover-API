cd1f5476b3e34ecdda16e3410de8f690ff1535c4
Josué Soto <deivysoto66@gmail.com>
2021-07-14 13:45:00 -0600

    Categories Model Comments

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -1,8 +1,21 @@
+/**
+ * Database connection
+ */
 const mongoose = require("mongoose");
+
+/**
+ * Schema creation
+ */
 const Schema = mongoose.Schema;
 
+/**
+ * Schema´s data
+ */
 const categories = new Schema ({
     nameCategory: {type: String}
 });
 
+/**
+ * Model
+ */
 module.exports = mongoose.model("categories", categories);
